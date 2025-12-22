2be309caf621ff186c43b9b9fa88b5cfa71b3f2f
jaddunani <jaddusuvarnakumar@gmail.com>
2025-12-22 14:56:30 +0530

    Init commit

---
+++ using list.py
@@ -0,0 +1,19 @@
+# def isprime(num):
+#     c=0
+#     for i in range(1,int(num**0.5)+1):
+#         if num%i==0:
+#             c+=1
+#     if c==1:
+#         return True
+# n=list(map(int,input().split()))
+# for i in n:
+#     if isprime(i):
+#         print(i,end=" ")
+
+# n=list(map(int,input().split()))
+# r=int(input())
+# sum=0
+# for i in range(r):
+#     sum+=n[i]
+# print(sum)
+
