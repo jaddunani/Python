7fba9de3389e676be81fe9b519fece2756b70350
jaddunani <jaddusuvarnakumar@gmail.com>
2025-12-30 12:06:23 +0530

    Init commit

--- using list.py
+++ using list.py
@@ -31,33 +31,57 @@
 #         a=b
 #         b=c
 
-n=list(map(int,input().split()))
-max=0
-min=0
-for i in n:
-    a=0
-    b=1
-    while True:
-        if i==b:
-            pass
-        elif b>i:
-            if b==i:
-                max=c
-            else:
-                max=b
-            if a==i:
-                min=b-a
-            else:
-                min=a
-            break
-        c=a+b
-        a=b
-        b=c
-    print(min,i,max,end=" =")
-    if i-min>max-i:
-        print(max)
-    elif i-min<max-i:
-        print(min)
-    else:
-        print(min,max)
-    print()
+# n=list(map(int,input().split()))
+# max=0
+# min=0
+# for i in n:
+#     a=0
+#     b=1
+#     while True:
+#         if i==b:
+#             pass
+#         elif b>i:
+#             if b==i:
+#                 max=c
+#             else:
+#                 max=b
+#             if a==i:
+#                 min=b-a
+#             else:
+#                 min=a
+#             break
+#         c=a+b
+#         a=b
+#         b=c
+#     print(min,i,max,end=" =")
+#     if i-min>max-i:
+#         print(max)
+#     elif i-min<max-i:
+#         print(min)
+#     else:
+#         print(min,max)
+#     print()
+
+
+# l=list(map(int,input().split()))
+# min=l[0]
+# for i in l:
+#     if i<min:
+#         min=i
+# c=0
+# while c<4:
+#     if min in l:
+#         pass
+#     else:
+#         print(min)
+#         c+=1
+#     min+=1
+
+
+# l=list(map(int,input().split()))
+# n=int(input())
+# for i in range(1,n+2):
+#     if i in l:
+#         pass
+#     else:
+#         print(i)
